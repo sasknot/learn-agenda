a49ac0552879d7863025859c1b1b1c65b676fb12
Rafael Silva <rafaelfsilva1@gmail.com>
2023-04-20 14:42:16 -0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,11 @@ import eslint from '@nabla/vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    __VUE_OPTIONS_API__: false,
+    __VUE_PROD_DEVTOOLS__: false
+  },
+
   plugins: [
     vue(),
     webfontDownload([
